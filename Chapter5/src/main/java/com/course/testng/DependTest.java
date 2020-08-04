910af9a53e63a10a0e00835a184942cb6c92672c
Your Name <you@example.com>
2020-08-04 09:36:41 +0800

    8.3.自动化测试学习代码

--- Chapter5/src/main/java/com/course/testng/DependTest.java
+++ Chapter5/src/main/java/com/course/testng/DependTest.java
@@ -1,6 +1,5 @@
 package com.course.testng;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.testng.annotations.Test;
 
 public class DependTest {
---
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,37 @@
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        //从配置文件中拼接测试的url
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = this.url+uri;
+        //测试逻辑代码书写
+        HttpGet get = new HttpGet(testUrl);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+
+    }
+}
